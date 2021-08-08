3156e1c7f862855ff16133d34be8601bd090f6f9
J3SUNG <wptjd6141@gmail.com>
2021-08-09 06:52:25 +0900

    Algorithm

--- BOJ/1339_단어_수학/1339_단어_수학.cpp
+++ BOJ/1339_단어_수학/1339_단어_수학.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
-
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-int main()
+int main (int argc, char *argv[])
 {
-    cout << "Hello, World!" << endl;
-
-    return 0;
+	int n;
+	int score = 9;
+	int index;
+	int result = 0;
+	vector<int> alp(26);
+	vector<string> v;
+	string str;
+	string text[10];
+	
+	cin >> n;
+	
+	for(int i=0; i<n; ++i){
+		cin >> str;
+		v.push_back(str);
+	}
+	
+	for(int i=0; i<n; ++i){
+		for(int j=0; j<v[i].length(); ++j){
+			index = v[i][j] - 'A';
+			alp[index] += pow(10, v[i].length() - j - 1);
+		}
+	}
+	
+	sort(alp.begin(), alp.end(), greater<int>());
+	
+	for(int i=0; i<10; ++i){
+		result += alp[i] * (9 - i);
+	}
+	
+	cout << result << endl;
+	
+	return 0;
 }
