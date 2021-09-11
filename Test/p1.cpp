9c1b520fe573ec436ece4283fd7f32028dc8a2f2
J3SUNG <wptjd6141@naver.com>
2021-09-11 14:45:49 +0900

    Algorithm

---
+++ Test/p1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+vector<string> split(string input, char delimiter) {
+    vector<string> answer;
+    stringstream ss(input);
+    string temp;
+ 
+    while (getline(ss, temp, delimiter)) {
+        answer.push_back(temp);
+    }
+ 
+    return answer;
+}
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+  int index;
+  string badUser;
+  string normalUser;
+  string prevStr = "";
+  vector<int> user(id_list.size());
+  vector<int> answer(id_list.size());
+  
+  sort(report.begin(), report.end());
+
+  for(int i=0; i<report.size(); ++i){
+    if(report[i] == prevStr){
+      continue;
+    }
+    badUser = split(report[i], ' ')[1];
+    index = find(id_list.begin(), id_list.end(), badUser) - id_list.begin();
+    ++user[index];
+    prevStr = report[i];
+  } 
+
+  for(int i=0; i<report.size(); ++i){
+    if(report[i] == prevStr){
+      continue;
+    }
+    badUser = split(report[i], ' ')[1];
+    index = find(id_list.begin(), id_list.end(), badUser) - id_list.begin();
+    if(user[index] >= k){
+      normalUser = split(report[i], ' ')[0];
+      index = find(id_list.begin(), id_list.end(), normalUser) - id_list.begin();
+      ++answer[index];
+      prevStr = report[i];
+    }
+  } 
+
+  return answer;
+}
+
+int main(int argc, char *argv[])
+{
+  int k = 2;
+  vector<string> id_list;
+  vector<string> report;
+  id_list.push_back("muzi");
+  id_list.push_back("frodo");
+  id_list.push_back("apeach");
+  id_list.push_back("neo");
+  report.push_back("muzi frodo");
+  report.push_back("apeach frodo");
+  report.push_back("muzi frodo");
+  report.push_back("frodo neo");
+  report.push_back("muzi neo");
+  report.push_back("apeach muzi");
+  
+  solution(id_list, report, k);
+
+  return 0;
+}
