5306ac35ba5a9ba5e3682b261d900c9a936f8785
J3SUNG <wptjd6141@naver.com>
2022-03-12 10:33:56 +0900

    Algorithm

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
 #include <string>
 #include <vector>
-#include <queue>
-#include <deque>
+
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-  int i = 0;
-  cout << "Hello, World!" << i << endl;
+int solution(int money, vector<int> costs) {
+    int answer = 0;
+    int value[6] = {1, 5, 10, 50, 100, 500};
+    bool use[6] = {true, true, true, true, true, true};
+    double goodCosts;
+    double arr[6] = {1, 5, 10, 50, 100, 500};
 
-  i = 1;
-  return 0;
+    for(int i=0; i<6; ++i){
+      arr[i] = costs[i] / arr[i];
+    } 
+    goodCosts = arr[0];
+    for(int i=1; i<6; ++i){
+      if(arr[i]>goodCosts){
+        use[i] = false;
+      } else {
+        goodCosts = arr[i];
+      }
+    }
+
+    for(int i=5; i>=0; --i){
+      if(use[i]){
+        answer += money / value[i] * costs[i] ;
+        money = money % value[i];
+      }
+    }
+
+    return answer;
 }
