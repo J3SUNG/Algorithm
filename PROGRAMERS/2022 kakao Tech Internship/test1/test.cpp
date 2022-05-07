1dccd4d24608de3b832bffd2a8f889793ec9b26a
J3SUNG <wptjd6141@naver.com>
2022-05-07 14:26:18 +0900

    algorithm

---
+++ PROGRAMERS/2022 kakao Tech Internship/test1/test.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<pair<int, int> > v = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
+
+void calc(char c, int num)
+{
+    switch(c){
+        case 'R':
+            v[0].first += num;
+            break;
+        case 'T':
+            v[0].second += num;
+            break;
+        case 'C':
+            v[1].first += num;
+            break;
+        case 'F':
+            v[1].second += num;
+            break;
+        case 'J':
+            v[2].first += num;
+            break;   
+        case 'M':  
+            v[2].second += num;
+            break; 
+        case 'A':
+            v[3].first += num;
+            break;
+        case 'N':
+            v[3].second += num;
+            break;
+    }
+}
+
+string solution(vector<string> survey, vector<int> choices) {
+    string answer = "";
+    string type = "RTCFJMAN";
+    // R T
+    // C F
+    // J M
+    // A N
+
+    for(int i=0; i<survey.size(); ++i){
+        if(choices[i] < 4) {
+            calc(survey[i][0], 4 - choices[i]);
+        } else if (choices[i] > 4){
+            calc(survey[i][1], choices[i] - 4);
+        }
+    }
+
+    for(int i=0; i<4; ++i){
+        if(v[i].first >= v[i].second){
+            answer += type[i * 2];
+        } else {
+            answer += type[i * 2 + 1];
+        }
+    }
+
+    for(int i=0; i<4; ++i) {
+      cout << v[i].first << " " << v[i].second << endl; 
+    }
+
+    return answer;
+}
