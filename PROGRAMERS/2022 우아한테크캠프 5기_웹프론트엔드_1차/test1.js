98b0838b625049ca9b0d628468ec42b06374c096
J3SUNG <wptjd6141@naver.com>
2022-05-08 13:22:13 +0900

    algorithm

---
+++ PROGRAMERS/2022 우아한테크캠프 5기_웹프론트엔드_1차/test1.js
@@ -0,0 +1,14 @@
+function solution(salaries, days) {
+  let answer = 0;
+  for (let i = 0; i < salaries.length; ++i) {
+    if (salaries[i][0] > days) {
+      answer += salaries[i];
+    } else if (days % salaries[i][0] == 0) {
+      answer += salaries[i][1] * parseInt(days / salaries[i][0]);
+    } else {
+      answer += salaries[i][1] * parseInt(days / salaries[i][0]);
+      answer += salaries[i];
+    }
+  }
+  return answer;
+}
