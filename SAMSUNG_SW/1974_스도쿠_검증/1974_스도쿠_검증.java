e9f6b22cbdc8ed089aefdf645179af2468a4daf4
J3SUNG <wptjd6141@naver.com>
2023-01-11 22:41:52 +0900

    Algorithm

---
+++ SAMSUNG_SW/1974_스도쿠_검증/1974_스도쿠_검증.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+ 
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        int T;
+        int width = 0; //511
+        int[] length = new int[9]; // 511
+        int[] square = new int[3]; // 511
+        String S;
+        boolean chk = false;
+         
+        Scanner sc = new Scanner(System.in);
+        T=sc.nextInt();
+        S=sc.nextLine();
+         
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            chk = false;
+                
+            for(int i=0; i<9; ++i){
+                S=sc.nextLine();
+                if(chk){ 
+                    continue;   
+                }
+                width = 0;
+                if(i%3==0){
+                    square[0] = 0;
+                    square[1] = 0;
+                    square[2] = 0;
+                }
+                if(i%9==0){
+                    length[0] = 0;   
+                    length[1] = 0;
+                    length[2] = 0;
+                    length[3] = 0;
+                    length[4] = 0;
+                    length[5] = 0;
+                    length[6] = 0;
+                    length[7] = 0;
+                    length[8] = 0;
+                }
+                for(int j=0; j<9; ++j){
+                    if((length[j] & (1 << (int)S.charAt(j*2)  - '0')) == 0){
+                        length[j] = length[j] | (1 << (int)(S.charAt(j*2) - '0'));   
+                    } else {
+                        chk = true;
+                        break;
+                    }
+                    if((width & (1 << (int)(S.charAt(j*2) - '0'))) == 0){
+                        width = width | (1 << (int)(S.charAt(j*2) - '0'));
+                    } else {
+                        chk = true;
+                        break;
+                    }
+                    if((square[j/3] & (1 << (int)(S.charAt(j*2) - '0'))) == 0){
+                        square[j/3] = square[j/3] | (1 << (int)(S.charAt(j*2) - '0'));
+                    } else {
+                        chk = true;
+                        break;
+                    }
+                }
+            }
+            if(chk){
+                System.out.println("#" + test_case +" 0");   
+            } else {
+                System.out.println("#" + test_case +" 1");
+            }
+        }
+    }
+}
