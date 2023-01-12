18c80b7afb37d56d6997b2ce3f26cdc9b4d80a6e
J3SUNG <wptjd6141@naver.com>
2023-01-12 21:04:39 +0900

    Algorithm

---
+++ SAMSUNG_SW/1959_두_개의_숫자열/1959_두_개의_숫자열.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+ 
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {       
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+         
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int ans = 0;
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int temp;
+            int sum;
+            int mul;
+            boolean chk = false;
+            if(n > m){
+                temp = n;
+                n = m;
+                m = temp;
+                chk = true;
+            }
+            int[] small = new int[n];
+            int[] big = new int[m];
+             
+            if(!chk){
+                for(int i=0; i<small.length; ++i){
+                    small[i] = sc.nextInt();   
+                }
+                for(int i=0; i<big.length; ++i){
+                    big[i] = sc.nextInt();   
+                }
+            } else {
+                for(int i=0; i<big.length; ++i){
+                    big[i] = sc.nextInt();   
+                }
+                for(int i=0; i<small.length; ++i){
+                    small[i] = sc.nextInt();   
+                }
+            }
+             
+            for(int i=0; i<m-n+1; ++i){
+                sum = 0;
+                for(int j=0; j<n; ++j){
+                    mul = small[j] * big[i+j];
+                    sum += mul;      
+                }                
+                //System.out.printf("%d ", sum);  
+                ans = Math.max(sum, ans);
+            }
+            System.out.printf("#%d %d%n", test_case, ans);
+        }
+    }
+}
