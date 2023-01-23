75d0c7b8c750ad1ba6bbf0cf8c45f5ce406ed470
J3SUNG <wptjd6141@naver.com>
2023-01-23 23:55:19 +0900

    Algorithm

---
+++ BOJ/15650_N과_M_(2)/15640_N과_M_(2).java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+  public static int[] arr;
+  public static boolean[] visit;
+  public static int n;
+  public static int m;
+  public static BufferedWriter bf;
+  public static void main(String argc[]) throws IOException{
+	bf = new BufferedWriter(new OutputStreamWriter(System.out));
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    m = sc.nextInt();
+    arr = new int[m];
+    visit = new boolean[n];
+    for(int i=0; i<n; ++i){
+      DFS(i, 0);
+    }
+    bf.close();
+  }
+
+  public static void DFS(int num, int cnt) throws IOException{
+    visit[num] = true;
+    
+    arr[cnt] = num + 1;
+    if(m == cnt+1){
+      for(int i=0; i<m; ++i) {
+    	  bf.write(arr[i] + " ");
+      }
+      bf.write("\n");
+      visit[num] = false;
+      return;
+    }
+
+    for(int i=num; i<n; ++i){
+      if(!visit[i]){
+        DFS(i, cnt+1);
+      }
+    }
+    
+    visit[num] = false;
+  }
+}
