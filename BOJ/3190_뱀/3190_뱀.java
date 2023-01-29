b03332eadbba5148ff40f0fb84ed40cefbbebdec
J3SUNG <wptjd6141@naver.com>
2023-01-29 16:09:12 +0900

    Algorithm

--- BOJ/3190_뱀/3190_뱀.java
+++ BOJ/3190_뱀/3190_뱀.java
@@ -1,10 +1,8 @@
-package com.ssafy.test2;
-
 import java.util.Scanner;
 import java.util.Queue;
 import java.util.LinkedList;
 
-public class test2 {
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
