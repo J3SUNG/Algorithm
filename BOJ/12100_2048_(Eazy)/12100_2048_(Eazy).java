f61afdeda5d8f0410ee58a72362d4ac3b1d69941
J3SUNG <wptjd6141@naver.com>
2023-01-30 01:47:12 +0900

    Algorithm

--- BOJ/12100_2048_(Eazy)/12100_2048_(Eazy).java
+++ BOJ/12100_2048_(Eazy)/12100_2048_(Eazy).java
@@ -35,14 +35,6 @@ public class Main {
 				}
 			}
 		}
-//		System.out.println(cnt);
-//		for(int i=0; i<n; ++i) {
-//			for(int j=0; j<n; ++j) {
-//				System.out.printf("%d ", newMap[i][j]);
-//			}
-//			System.out.println();
-//		}
-//		System.out.println();
 		if(max == cnt) {
 			return;
 		}
