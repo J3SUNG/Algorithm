33e9482b122e72ac5e09136bf5d391f98d59b608
J3SUNG <wptjd6141@naver.com>
2023-02-06 00:57:53 +0900

    Algorithm

--- BOJ/19237_어른상어/19237_어른상어.java
+++ BOJ/19237_어른상어/19237_어른상어.java
@@ -10,11 +10,10 @@ public class test21 {
 	static int n, m, k;
 	static int[][] map;
 	static int[][] time;
-	static int[][] sharks;
 	static int[][][] sharkMove;
+	static Shark[] shark;
 	static int[] dy = { 0, -1, 1, 0, 0 };
 	static int[] dx = { 0, 0, 0, -1, 1 };
-	static int bit = 0;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,7 +27,7 @@ public class test21 {
 
 		map = new int[n + 1][n + 1];
 		time = new int[n + 1][n + 1];
-		sharks = new int[m + 1][3];
+		shark = new Shark[m + 1];
 		sharkMove = new int[m + 1][5][4];
 
 		for (int i = 1; i <= n; i++) {
@@ -37,17 +36,15 @@ public class test21 {
 				int num = Integer.parseInt(st.nextToken());
 
 				if (num > 0) {
-					sharks[num][0] = i;
-					sharks[num][1] = j;
-					sharks[num][2] = 0;
+					shark[num] = new Shark(i, j, 0);
 					time[i][j] = k;
-					map[i][j] = n;
+					map[i][j] = num;
 				}
 			}
 		}
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= m; i++)
-			sharks[i][2] = Integer.parseInt(st.nextToken());
+			shark[i].d = Integer.parseInt(st.nextToken());
 
 		for (int i = 1; i <= m; i++) {
 			for (int j = 1; j <= 4; j++) {
@@ -66,12 +63,18 @@ public class test21 {
 	public static int solve() {
 
 		int ans = 0;
-
+		
 		while (true) {
-			if(bit == (1<<m+1)-4) {
+			int count = 0;
+			for(int i=1; i<=m; ++i) {
+				if(shark[i] != null) {
+					++count;
+				}
+			}
+			if(count == 1) {
 				return ans;
 			}
-
+			
 			if (ans >= 1000) {
 				return -1;
 			}
@@ -80,12 +83,11 @@ public class test21 {
 
 			for (int i = 1; i <= m; i++) {
 
-				if ((bit & (1<<i)) == 0) { // 상어가 경계 안에 있다면
+				if (shark[i] != null) {
 					move(tmp, i);
 				}
 			}
 
-			// 냄새 유효기간 하나씩 줄이기
 			for (int i = 1; i <= n; i++) {
 				for (int j = 1; j <= n; j++) {
 					if (time[i][j] > 0)
@@ -96,7 +98,6 @@ public class test21 {
 				}
 			}
 
-			// 이동후의 상어 위치의 냄새 정보와 유효기간 초기화하기
 			for (int i = 1; i <= n; i++) {
 				for (int j = 1; j <= n; j++) {
 					if (tmp[i][j] > 0) {
@@ -116,39 +117,46 @@ public class test21 {
 		int d = 0;
 		boolean chk = false;
 
-		// 1-1. 높은 우선순위부터 차례대로 탐색
 		for (int i = 0; i < 4; i++) {
-			d = sharkMove[index][sharks[index][2]][i];
-			y = sharks[index][0] + dy[d];
-			x = sharks[index][1] + dx[d];
+			d = sharkMove[index][shark[index].d][i];
+			y = shark[index].y + dy[d];
+			x = shark[index].x + dx[d];
 
-			// 경계를 벗어나지 않으면서, 냄새가 없는 곳을 찾으면 break로 빠져나옴
 			if ((1 <= y && y <= n) && (1 <= x && x <= n) && map[y][x] == 0) {
 				chk = true;
 				break;
 			}
 		}
 
-		// 1-2. 냄새가 없는 곳이 없는 경우
 		if (!chk) {
 			for (int i = 0; i < 4; i++) {
-				d = sharkMove[index][sharks[index][2]][i];
-				y = sharks[index][0] + dy[d];
-				x = sharks[index][1] + dx[d];
+				d = sharkMove[index][shark[index].d][i];
+				y = shark[index].y + dy[d];
+				x = shark[index].x + dx[d];
 
-				if ((1 <= y && y <= n) && (1 <= x && x <= n) && map[y][x] == m)
+				if ((1 <= y && y <= n) && (1 <= x && x <= n) && map[y][x] == index)
 					break;
 			}
 		}
 
 		if (tmp[y][x] == 0) {
 
-			tmp[y][x] = m;
-			sharks[index][0] = y;
-			sharks[index][1] = x;
-			sharks[index][2] = d;
+			tmp[y][x] = index;
+			shark[index].y = y;
+			shark[index].x = x;
+			shark[index].d = d;
 		} else {
-			bit = bit | (1 << index);
+			shark[index] = null;
+		}
+	}
+	static class Shark {
+
+		int y, x, d;
+
+		Shark(int y, int x, int d) {
+			this.y = y;
+			this.x = x;
+			this.d = d;
 		}
 	}
 }
