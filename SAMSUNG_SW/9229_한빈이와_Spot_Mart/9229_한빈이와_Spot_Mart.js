256d7e4c0e83a73c2d1bbd7c28b4f995b6f69ab9
J3SUNG <wptjd6141@naver.com>
2023-02-14 22:21:22 +0900

    Algorithm

---
+++ SAMSUNG_SW/9229_한빈이와_Spot_Mart/9229_한빈이와_Spot_Mart.js
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+ 
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        for(int tc=0; tc<t; ++tc) {
+            int ans = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int[] arr = new int[n];
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<n; ++i) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            for(int i=0; i<n; ++i) {
+                for(int j=i+1; j<n; ++j) {
+                    if(m >= arr[i] + arr[j]) {
+                        ans = Math.max(ans, arr[i] + arr[j]);
+                    }
+                }
+            }
+            ans = ans==0?-1:ans;
+            bw.write("#" + (tc+1) + " " + ans + "\n");
+        }
+        bw.close();
+    }
+}
