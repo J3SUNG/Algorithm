06bcfa6d89f94fd49ee77ed07e5fd90773ed21c5
J3SUNG <wptjd6141@naver.com>
2023-03-04 16:32:57 +0900

    Algorithm

---
+++ BOJ/11055_가장_큰_증가_부분_수열/11055_가장_큰_증가_부분_수열.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = null;
+		
+		int ans = 0;
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n+1];
+		int[] DP = new int[n+1];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=n; ++i) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			DP[i] = arr[i];
+		}
+		
+		for(int i=0; i<=n; ++i) {
+			for(int j=i+1; j<=n; ++j) {
+				if(arr[i] < arr[j]) {  
+					DP[j] = Math.max(DP[j], arr[j] + DP[i]);
+				}
+			}
+		}
+		
+		for(int i=1; i<=n; ++i) {
+			ans = Math.max(ans, DP[i]);
+		}		
+		
+		bw.write(ans + "\n");		
+		bw.close();
+	}
+}
