955d27250432955991e8d6c02b052c23c2f8b796
J3SUNG <wptjd6141@naver.com>
2023-03-05 11:54:19 +0900

    Algorithm

--- BOJ/14003_가장_긴_증가하는_부분_수열_5/14003_가장_긴_증가하는_부분_수열_5.java
+++ BOJ/14003_가장_긴_증가하는_부분_수열_5/14003_가장_긴_증가하는_부분_수열_5.java
@@ -15,8 +15,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = null;
-		
+		StringTokenizer st = null;		
 		n = Integer.parseInt(br.readLine());
 		arr = new int[n];
 		dp = new ArrayList<>();
