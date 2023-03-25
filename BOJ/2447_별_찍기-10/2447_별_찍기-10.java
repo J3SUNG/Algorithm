f506173f03f788af8b0b739e615e9beacd0d6dd1
wptjd6141@naver.com <wptjd6141@naver.com>
2023-03-25 23:43:04 +0900

    Algorithm

--- BOJ/2447_별_찍기-10/2447_별_찍기-10.java
+++ BOJ/2447_별_찍기-10/2447_별_찍기-10.java
@@ -1,5 +1,3 @@
-package test;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -7,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class test {
+public class Main {
 	static char[][] map;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
