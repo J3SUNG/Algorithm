d686267c89481f84efbe85cb855eab84d1a29b1c
wptjd6141@naver.com <wptjd6141@naver.com>
2023-03-26 23:30:44 +0900

    Algorithm

---
+++ PROGRAMERS/2019_카카오_개발자_겨울_인턴십/징검다리_건너기/징검다리_건너기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] stones, int k) {
+        int answer = 0;
+        int left = 987654321;
+        int right = -1;
+        int mid;
+        for(int i=0; i<stones.length; ++i){
+            left = Math.min(left, stones[i]);
+            right = Math.max(right, stones[i]);
+        }
+
+        while(left <= right){
+            mid = (left+right) / 2;
+            int count = 0;
+
+            for(int i=0; i<stones.length; ++i){
+                if(count<k){
+                    if(stones[i] <= mid){
+                        ++count;
+                    } else {
+                        count = 0;
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            if(count >= k){
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return answer;
+    }
+}
