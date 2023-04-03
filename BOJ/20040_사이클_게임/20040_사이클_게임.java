5fa0827c4b5f8a3292e889301efb7c03e6bf6d57
wptjd6141@naver.com <wptjd6141@naver.com>
2023-04-03 22:38:28 +0900

    Algorithm

--- BOJ/20040_사이클_게임/20040_사이클_게임.java
+++ BOJ/20040_사이클_게임/20040_사이클_게임.java
@@ -11,27 +11,27 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		int ans = 0;
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		p = new int[n];
-		
-		for(int i=0; i<n; ++i) {
+
+		for (int i = 0; i < n; ++i) {
 			p[i] = i;
 		}
-		
-		for(int i=1; i<=m; ++i) {
+
+		for (int i = 1; i <= m; ++i) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 
-			if(find(a) == find(b)) {
+			if (find(a) == find(b)) {
 				ans = i;
 				break;
 			}
-			
-			if(p[a] < p[b]) {
+
+			if (p[a] < p[b]) {
 				p[p[b]] = p[a];
 			} else {
 				p[p[a]] = p[b];
@@ -40,12 +40,12 @@ public class Main {
 		bw.write(ans + "\n");
 		bw.close();
 	}
-	
+
 	public static int find(int num) {
-		if(num == p[num]) {
+		if (num == p[num]) {
 			return num;
 		}
-		
+
 		int result = find(p[num]);
 		p[num] = result;
 		return result;
