f6a9c6a23615dc45d478b9769de6c18f112460a3
wptjd6141@naver.com <wptjd6141@naver.com>
2023-07-20 23:33:44 +0900

    Algorithm BOJ 3343

---
+++ BOJ/1000~9999/3343_장미/tempCodeRunnerFile.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    long n = Long.parseLong(st.nextToken());
+    long a = Long.parseLong(st.nextToken());
+    long b = Long.parseLong(st.nextToken());
+    long c = Long.parseLong(st.nextToken());
+    long d = Long.parseLong(st.nextToken());
+    long result = Long.MAX_VALUE;
+
+    long result1 = (n / a) * b;
+    long mod1 = n % a;
+    result1 += Math.min((mod1 + a - 1) / a * b, ((mod1 + c - 1) / c) * d);
+
+    long result2 = (n / c) * d;
+    long mod2 = n % c;
+    result2 += Math.min((mod2 + a - 1) / a * b, ((mod2 + c - 1) / c) * d);
+
+    result = Math.min(result1, result2);
+
+    bw.write(result + "\n");
+    bw.close();
+  }
+}
