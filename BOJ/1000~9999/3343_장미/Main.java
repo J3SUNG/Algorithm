354bac192ca42d65f7a51f17fb32425e7eec1c20
wptjd6141@naver.com <wptjd6141@naver.com>
2023-07-21 20:21:03 +0900

    Algorithm BOJ 3343

--- BOJ/1000~9999/3343_장미/Main.java
+++ BOJ/1000~9999/3343_장미/Main.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
