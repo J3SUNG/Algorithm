572da717f2c43f7c10f5f4af27d1b01099acadb2
wptjd6141@naver.com <wptjd6141@naver.com>
2023-07-23 16:33:33 +0900

    Algorithm BOJ 13023

--- BOJ/10000~19999/13023_ABCDE/Main.java
+++ BOJ/10000~19999/13023_ABCDE/Main.java
@@ -11,48 +11,56 @@ public class Main {
 	static int m;
 	static int[] visit;
 	static ArrayList<ArrayList<Integer>> arr;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		visit = new int[n];
 		arr = new ArrayList<>();
-		for(int i=0; i<n; ++i) {
+
+		for (int i = 0; i < n; ++i) {
 			ArrayList<Integer> temp = new ArrayList<>();
 			arr.add(temp);
 		}
-		for(int i=0; i<m; ++i) {			
+
+		for (int i = 0; i < m; ++i) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			arr.get(a).add(b);
 			arr.get(b).add(a);
 		}
-		
-		for(int i=0; i<n; ++i) {
+
+		for (int i = 0; i < n; ++i) {
 			visit[i] = 1;
 			DFS(i, 1);
 			visit[i] = 0;
 		}
-		
+
 		bw.write(ans + "");
 		bw.close();
 	}
+
 	public static void DFS(int index, int cnt) {
-		if(cnt == 5 || ans == 1) {
+		if (cnt == 5 || ans == 1) {
 			ans = 1;
 			return;
 		}
-		for(int i=0; i<arr.get(index).size(); ++i) {
+
+		for (int i = 0; i < arr.get(index).size(); ++i) {
 			int nextIndex = arr.get(index).get(i);
-			if(visit[nextIndex] == 1) {
+
+			if (visit[nextIndex] == 1) {
 				continue;
 			}
+
 			visit[nextIndex] = 1;
-			DFS(nextIndex, cnt+1);
-			visit[nextIndex] = 0;			
+			DFS(nextIndex, cnt + 1);
+			visit[nextIndex] = 0;
 		}
 	}
 }
