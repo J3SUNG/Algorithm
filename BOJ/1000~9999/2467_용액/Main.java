9617678a195f3d302a604e9bb3ab4c232ef66c36
wptjd6141@naver.com <wptjd6141@naver.com>
2023-07-24 21:30:44 +0900

    Algorithm BOJ 2467

---
+++ BOJ/1000~9999/2467_용액/Main.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int result = 0;
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    int chk = 0;
+    int goodSum = 2_000_000_001;
+    int goodLeft = 0;
+    int goodRight = 0;
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; ++i) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int left = 0;
+    int right = n - 1;
+    while (left < right) {
+      int sum = Math.abs(arr[left] + arr[right]);
+      if (goodSum > sum) {
+        goodSum = sum;
+        goodLeft = left;
+        goodRight = right;
+      }
+
+      if (arr[left] + arr[right] > 0) {
+        --right;
+      } else {
+        ++left;
+      }
+    }
+
+    bw.write(arr[goodLeft] + " " + arr[goodRight] + "\n");
+    bw.close();
+  }
+}
