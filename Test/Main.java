640472da32036459dd450a0b10d432c607b08de6
wptjd6141@naver.com <wptjd6141@naver.com>
2023-08-06 18:22:13 +0900

    Algorithm

--- Test/Main.java
+++ Test/Main.java
@@ -1,79 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class Main {
-  static int M, N, L;
-  static int[] x;
-  static Loc[] animal;
-  static int count = 0;
-
-  static class Loc {
-    int x, y;
-
-    public Loc(int x, int y) {
-      this.x = x;
-      this.y = y;
-    }
-
-  }
+  static long N, M;
+  static long[] time;
+  static long answer = 1_000_000_000L * 1_000_000_000L;
 
   public static void main(String[] args) throws Exception {
+
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-
-    M = Integer.parseInt(st.nextToken());
-    N = Integer.parseInt(st.nextToken());
-    L = Integer.parseInt(st.nextToken());
-    x = new int[M];
-
+    StringTokenizer st = null;
     st = new StringTokenizer(br.readLine());
-
-    for (int i = 0; i < M; i++) {
-      x[i] = Integer.parseInt(st.nextToken());
-    }
-
-    Arrays.sort(x);
-
-    animal = new Loc[N];
-
+    N = Long.parseLong(st.nextToken());
+    M = Long.parseLong(st.nextToken());
+    time = new long[(int) N];
     for (int i = 0; i < N; i++) {
-      st = new StringTokenizer(br.readLine());
-      animal[i] = new Loc(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+      time[i] = Long.parseLong(br.readLine());
     }
-
-    Arrays.sort(animal, (o1, o2) -> o1.x - o2.x);
-
-    for (int i = 0; i < N; i++) {
-      int l = 0;
-      int r = M - 1;
-      int m = (l + r) / 2;
-
-      if (animal[i].y > L) {
-        continue;
-      }
-
-      while (l <= r) {
-
-        if (Math.abs(animal[i].x - x[m]) <= L - animal[i].y) {
-          count++;
-          break;
-        }
-
-        if (animal[i].x - x[m] > 0) {
-          l = m + 1;
-          m = (l + r) / 2;
-          continue;
-        } else if (animal[i].x - x[m] < 0) {
-          r = m - 1;
-          m = (l + r) / 2;
-          continue;
-        } else if (animal[i].x == x[m]) {
+    long start = 1;
+    long end = 1_000_000_000L * 1_000_000_000L;
+    while (start <= end) {
+      long mid = (start + end) / 2;
+      long temp = M;
+      for (int i = 0; i < N; i++) {
+        temp -= mid / time[i];
+        if (temp <= 0) {
           break;
         }
       }
+      if (temp <= 0) {
+        end = mid - 1;
+        answer = Math.min(answer, mid);
+      } else {
+        start = mid + 1;
+      }
     }
-
-    System.out.println(count);
-
+    System.out.println(answer);
   }
 }
