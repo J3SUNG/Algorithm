803674366f0ab8a497692a77f27fa4eef8d0d4a5
wptjd6141@naver.com <wptjd6141@naver.com>
2023-08-19 00:18:45 +0900

    Feat: Algorithm

--- BOJ/10000~19999/16434_드래곤_앤_던전/Main.java
+++ BOJ/10000~19999/16434_드래곤_앤_던전/Main.java
@@ -9,8 +9,7 @@ public class Main {
     StringTokenizer st = new StringTokenizer(br.readLine());
 
     int n = Integer.parseInt(st.nextToken());
-    double a = Long.parseLong(st.nextToken());
-    double result = 9e18;
+    Long a = Long.parseLong(st.nextToken());
 
     int[][] map = new int[n][3];
     for (int i = 0; i < n; i++) {
@@ -20,39 +19,36 @@ public class Main {
       map[i][2] = Integer.parseInt(st.nextToken());
     }
 
-    double left = 1;
-    double right = 9e18;
+    Long left = 1L;
+    Long right = 999999000001L * n;
+    result = 999999000001L * n;
     while (left <= right) {
-      double attack = a;
-      double maxHp = Math.floor((left + right) / 2);
-      double hp = Math.floor(maxHp);
+      Long attack = a;
+      Long maxHp = (left + right) / 2;
+      Long hp = maxHp;
       boolean isKill = true;
 
       for (int i = 0; i < n; ++i) {
         if (map[i][0] == 1) {
-          hp -= map[i][1] * Math.floor((map[i][2] - 1) / attack);
+          hp -= map[i][1] * ((map[i][2] - 1) / attack);
           if (hp <= 0) {
             isKill = false;
             break;
           }
         } else if (map[i][0] == 2) {
           attack += map[i][1];
-          hp += map[i][2];
-          if (hp > maxHp) {
-            hp = maxHp;
-          }
+          hp = Math.min(hp + map[i][2], maxHp);
         }
       }
 
       if (isKill) {
-        result = Math.min(result, maxHp);
         right = maxHp - 1L;
       } else {
         left = maxHp + 1L;
       }
     }
 
-    bw.write(String.format("%.0f", result) + "");
+    bw.write(left + "");
     bw.close();
   }
 }
