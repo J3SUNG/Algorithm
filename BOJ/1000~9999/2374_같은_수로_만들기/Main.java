fba2ca0a7d530d52316158339b207543d06d22d0
wptjd6141@naver.com <wptjd6141@naver.com>
2023-08-20 23:31:04 +0900

    Algorithm

--- BOJ/1000~9999/2374_같은_수로_만들기/Main.java
+++ BOJ/1000~9999/2374_같은_수로_만들기/Main.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
