754907bd45072385fec5310c961ed5a336e08a08
wptjd6141@naver.com <wptjd6141@naver.com>
2023-11-05 20:25:36 +0900

    Feat : 5582 공통 부분 문자열

---
+++ BOJ/1000~9999/5582_공통_부분_문자열/Main.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  static String a;
+  static String b;
+  static int[][] dp;
+  static int result;
+
+  public static void main(String[] args) throws Exception {
+    simulation();
+  }
+
+  public static void simulation() throws Exception {
+    init();
+    calc();
+    print();
+  }
+
+  public static void init() throws Exception {
+    a = br.readLine();
+    b = br.readLine();
+    result = 0;
+    dp = new int[a.length() + 1][b.length() + 1];
+  }
+
+  public static void print() throws Exception {
+    bw.write(result + "\n");
+    bw.close();
+  }
+
+  public static void calc() throws Exception {
+    for (int i = 1; i <= a.length(); ++i) {
+      for (int j = 1; j <= b.length(); ++j) {
+        if (a.charAt(i - 1) == b.charAt(j - 1)) {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+        }
+      }
+    }
+
+    for (int i = 1; i <= a.length(); ++i) {
+      for (int j = 1; j <= b.length(); ++j) {
+        result = Math.max(result, dp[i][j]);
+      }
+    }
+  }
+}
