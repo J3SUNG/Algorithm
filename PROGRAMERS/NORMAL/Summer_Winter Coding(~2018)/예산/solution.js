c59583d6266d3c3d18e419b8a2da86f91d6773cb
wptjd6141@naver.com <wptjd6141@naver.com>
2025-01-04 14:46:17 +0900

    Algorithm: 예산 내 최대 지원 부서 수 계산 (Greedy)
    - 부서의 지원 요청 금액을 오름차순으로 정렬
    - 작은 금액부터 예산을 초과하지 않을 때까지 합산
    - 예산을 초과하면 해당 시점의 부서 수 반환

---
+++ PROGRAMERS/NORMAL/Summer_Winter Coding(~2018)/예산/solution.js
@@ -0,0 +1,14 @@
+function solution(d, budget) {
+  d.sort((a, b) => a - b);
+
+  let sum = 0;
+  for (let i = 0; i < d.length; ++i) {
+    sum += d[i];
+
+    if (sum > budget) {
+      return i;
+    }
+  }
+
+  return d.length;
+}
