a69d91dcc9770292fd350f3f32f9eb9dff9f5968
wptjd6141@naver.com <wptjd6141@naver.com>
2025-01-05 22:49:58 +0900

    Algorithm: 27535번 제주 초콜릿 지키기 (구현)
    - 초콜릿 종류별 남은 개수를 객체로 관리하여 데이터 처리
    - 초콜릿 소비 데이터를 순차적으로 적용하여 남은 개수 업데이트
    - 남은 초콜릿을 개수와 정렬 우선순위에 따라 정렬
    - 총합을 기반으로 진법 변환하여 출력 형식 생성
    - 남은 초콜릿이 없을 경우 "NULL" 출력 처리

---
+++ BOJ/20000~29999/27535_제주_초콜릿_지키기/solution.ts
@@ -0,0 +1,53 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input: string[] = [];
+
+readline
+  .on("line", function (line: string) {
+    input.push(line);
+  })
+  .on("close", function () {
+    interface Chocolate {
+      num: number;
+      index: number;
+      text: string;
+    }
+
+    const order: number[] = [2, 4, 0, 3, 1];
+    const labels: string[] = ["H", "T", "C", "K", "G"];
+    const list: Chocolate[] = input[0].split(" ").map((num, index) => {
+      return {
+        num: parseInt(num),
+        index: order[index],
+        text: labels[index],
+      };
+    });
+
+    const eatList: number[][] = input.slice(2).map((line) => line.split(" ").map(Number));
+
+    let sum: number = list.reduce((a, c) => a + c.num, 0);
+    let base: number = sum % 10 <= 1 ? 10 : sum % 10;
+
+    for (const eat of eatList) {
+      sum = list.reduce((a, c, i) => {
+        c.num -= eat[i];
+        return a + c.num;
+      }, 0);
+
+      const orderStr: string = list
+        .filter((item) => item.num > 0)
+        .sort((a, b) => (b.num === a.num ? a.index - b.index : b.num - a.num))
+        .map((item) => item.text)
+        .join("");
+
+      console.log(`${sum.toString(base)}7H`);
+      console.log(orderStr || "NULL");
+
+      base = sum % 10 <= 1 ? 10 : sum % 10;
+    }
+
+    process.exit();
+  });
