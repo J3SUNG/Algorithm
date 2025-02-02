7f78b6fec860f94b342a370eaaacb01705ef8d91
wptjd6141@naver.com <wptjd6141@naver.com>
2025-02-03 06:07:38 +0900

    Algorithm: 문자열 분할 (문자열, 그리디 알고리즘) - 기준 문자를 설정하고 등장 횟수를 카운트 - 기준 문자와 동일하면 증가, 다르면 감소 - 카운트가 0이 되면 분할 횟수 증가 및 기준 문자 초기화 - 마지막 기준 문자가 남아있다면 추가 처리

---
+++ PROGRAMERS/NORMAL/문자열_나누기/solution.js
@@ -0,0 +1,20 @@
+function solution(s) {
+  let answer = 0;
+  let cnt = 0;
+  let base = "";
+
+  for (const char of s) {
+    base ||= char;
+
+    count += char === base ? 1 : -1;
+
+    if (count === 0) {
+      ++answer;
+      base = "";
+    }
+  }
+
+  if (base) ++answer;
+
+  return answer;
+}
