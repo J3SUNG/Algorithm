71b36931ac61457f7469c15893509161b30f8e7c
wptjd6141@naver.com <wptjd6141@naver.com>
2025-04-11 12:58:35 +0900

    Algorithm: N개의 최소공배수 (수학) - 유클리드 호제법을 사용해 두 수의 최대공약수를 계산 - 최대공약수를 기반으로 두 수의 최소공배수를 계산 - 배열의 모든 원소에 대해 reduce를 사용해 최소공배수를 순차적으로 계산

---
+++ PROGRAMERS/NORMAL/N개의_최소공배수/solution.js
@@ -0,0 +1,16 @@
+function gcd(a, b) {
+  while (b !== 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
+
+function solution(arr) {
+  return arr.reduce((a, c) => lcm(a, c));
+}
